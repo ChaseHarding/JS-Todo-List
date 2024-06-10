8ef919f9c2b6ed36b1547091f735a927c18de9a0
ChaseHarding <chaseharding965@gmail.com>
2024-06-09 23:28:33 -0700

    js logic for dynamically added list items from user inputs and clearing the todo list input after adding a todo item

--- app.js
+++ app.js
@@ -3,11 +3,35 @@ document.addEventListener('DOMContentLoaded', () => {
     const todoForm = document.getElementById('todo-form');
     const todoInput = document.getElementById('todo-input');
     const todoList = document.getElementById('todo-list');
+
     // function for adding new todo items
+    function addTodoItem(e) {
+        e.preventDefault(); //preventing default page reloading
 
-    // create a complere button for the item
+        const taskText = todoInput.value.trim(); //trim will remove any extra spaces or line breaks the user may accidently include
+        
+        if(taskText !== '') {
 
-    // append the complete button to the todo item
+            const li = document.createElement('li');
+            li.textContent = taskText; //now the text content will be whatever the user has inputted
+
+            // create a complete button for the item
+            const completeButton = document.createElement('button');
+            completeButton.textContent = 'Complete';
+            completeButton.classList.add('complete');
+
+            completeButton.addEventListener('click', () => {
+                li.classList.toggle('completed')
+            });
+
+              // append the complete button to the todo item
+            li.appendChild(completeButton)//appending the complete button to the li
+            todoList.appendChild(li);//appending the li to the todo list
+
+            todoInput.value = '';
+        }
+    }
 
     // add an event listener to the form to handle submission
+    todoForm.addEventListener('submit', addTodoItem)
 })
