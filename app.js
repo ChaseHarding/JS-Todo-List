b693b81f5d8fe593269949701a745b234015d9bd
ChaseHarding <chaseharding965@gmail.com>
2024-06-09 22:57:00 -0700

    planning the js

---
+++ app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Select the required elements
+
+    // function for adding new todo items
+
+    // create a complere button for the item
+
+    // append the complete button to the todo item
+
+    // add an event listener to the form to handle submission
+})
